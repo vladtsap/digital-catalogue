89cf492648e813285989c27a993197dc83162a0a
Vladyslav <vldzpp@gmail.com>
2018-12-13 16:09:42 +0200

    add Groups

--- LGA/lgaapp/admin.py
+++ LGA/lgaapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Subject, Art
+from .models import Subject, Art, Group
 # Register your models here.
 
 class SubjectAdmin(admin.ModelAdmin):
@@ -10,6 +10,10 @@ class SubjectAdmin(admin.ModelAdmin):
 class ArtAdmin(admin.ModelAdmin):
     list_display = ['name', 'country']
 
+class GroupAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name', )}
 
 admin.site.register(Subject, SubjectAdmin)
 admin.site.register(Art, ArtAdmin)
+admin.site.register(Group, GroupAdmin)
--- LGA/lgaapp/models.py
+++ LGA/lgaapp/models.py
@@ -2,28 +2,46 @@ from django.db import models
 
 # Create your models here.
 class Subject(models.Model):
-    name = models.CharField(max_length=200, db_index=True, unique=True)
-    slug = models.SlugField(max_length=200, db_index=True, unique=True)
-    #reeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+	name = models.CharField(max_length=200, db_index=True, unique=True)
+	slug = models.SlugField(max_length=200, db_index=True, unique=True)
+	#reeeeeeeeeeeeeeeeeeeeeeeeeeeeee
 
 
-    class Meta:
-        ordering = ['name']
-        verbose_name = 'Тематика'
-        verbose_name_plural = 'Тематики'
+	class Meta:
+		ordering = ['name']
+		verbose_name = 'Тематика'
+		verbose_name_plural = 'Тематики'
 
-    def __str__(self):
-        return self.name
+	def __str__(self):
+		return self.name
 
 
 class Art(models.Model):
-    name = models.CharField(max_length=200, db_index=True, unique=True)
-    country = models.CharField(max_length=200, db_index=True, unique=True)
+	name = models.CharField(max_length=200, db_index=True, unique=True)
+	country = models.CharField(max_length=200, db_index=True, unique=True)
 
-    class Meta:
-        ordering = ['country']
-        verbose_name = 'Мистецтво'
-        verbose_name_plural = 'Мистецтво'
+	class Meta:
+		ordering = ['country']
+		verbose_name = 'Мистецтво'
+		verbose_name_plural = 'Мистецтво'
 
-    def __str__(self):
-        return str(self.name) + str(self.country)
+	def __str__(self):
+		return str(self.name) + str(self.country)
+
+class Group(models.Model):
+	GROUP_CHOICES = (
+		(u'VI', u'Вітчизняні'),
+		(u'IN', u'Іноземні'),
+		(u'PI', u'Періодика'),
+		(u'CA', u'Каталог'),
+	)
+	name = models.CharField(max_length=200, choices=GROUP_CHOICES, db_index=True)
+	slug = models.SlugField(max_length=200, db_index=True, unique=True)
+
+	class Meta:
+		ordering = ['name']
+		verbose_name = 'Групи'
+		verbose_name_plural = 'Групи'
+
+	def __str__(self):
+		return self.name
