421182aff47950c08ed60513854a83e556eafbaf
George Brusentsov <32494347+Medezee@users.noreply.github.com>
2018-12-13 15:43:54 +0200

    Added category Art to models

--- LGA/lgaapp/models.py
+++ LGA/lgaapp/models.py
@@ -14,3 +14,15 @@ class Subject(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Art(models.Model):
+    name = models.CharField(max_length=200, db_index=True, unique=True)
+    country = models.CharField(max_length=200, db_index=True, unique=True)
+
+    class Meta:
+        ordering = ['country']
+        verbose_name = 'Мистецтво'
+
+    def __str__(self):
+        return str(self.name) + str(self.country)
