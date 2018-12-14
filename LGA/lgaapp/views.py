7bb7adf4807105ba72aca939b618a6350db3635c
Vladyslav <vldzpp@gmail.com>
2018-12-14 13:46:51 +0200

    created html and started work

--- LGA/lgaapp/views.py
+++ LGA/lgaapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from lgaapp.models import Art
 
 # Create your views here.
+def search(request):
+	arts = Art.objects.filter(name='Графіка')
+
+	return render(request, 'search/index.html', {'arts':arts})
