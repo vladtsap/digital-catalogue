d6ce295b2e8c3ebb4320b9e859ab4427a8dd4195
Vladyslav <vldzpp@gmail.com>
2018-12-14 15:19:49 +0200

    improvements

--- LGA/lgaapp/views.py
+++ LGA/lgaapp/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
 from lgaapp.models import Art
 
+q = [1,2,3]
 # Create your views here.
 def search(request):
 	arts = Art.objects.filter(name='Графіка')
 
-	return render(request, 'search/index.html', {'arts':arts})
+	return render(request, 'search/index.html', {'arts':arts, 'q':q})
+
+
