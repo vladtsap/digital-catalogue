d0cc7e75bc6e4447a95480a7620430d2abcea4e7
Misha Filipenko <mishafilipenko@ukr.net>
2018-12-14 16:06:57 +0200

    book

--- LGA/lgaapp/admin.py
+++ LGA/lgaapp/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Subject, Art, Group
+from .models import Subject, Art, Group, Book
 # Register your models here.
 
 class SubjectAdmin(admin.ModelAdmin):
@@ -15,6 +15,11 @@ class GroupAdmin(admin.ModelAdmin):
     list_display = ['name', 'slug']
     prepopulated_fields = {'slug': ('name', )}
 
+class BookAdmin(admin.ModelAdmin):
+    list_display = ['name','slug']
+    prepopulated_fields = {'slug': ('name', 'isbn', )}
+
+admin.site.register(Book, BookAdmin)
 admin.site.register(Subject, SubjectAdmin)
 admin.site.register(Art, ArtAdmin)
 admin.site.register(Group, GroupAdmin)
