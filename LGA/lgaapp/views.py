d8c5c276103ca6d172c03c8eaa5fda20725268f1
George Brusentsov <32494347+Medezee@users.noreply.github.com>
2019-02-13 21:27:28 +0200

    Improved search func
    Now can search similar objects and add case-insensitivity

--- LGA/lgaapp/views.py
+++ LGA/lgaapp/views.py
@@ -6,30 +6,30 @@ from .forms import AddBook
 
 
 def BookView(request, pk):
-	books = Book.objects.filter(slug=pk)
-	# arts = [Art.objects.get(pk=x['art']) for x in books.values('art') if x['art'] != None]
-	# if Art.objects.get(pk=books.values('art')['art']) != None:
-	#     arts = [Art.objects.get(pk=x['art']) for x in books.values('art')]
+    books = Book.objects.filter(slug=pk)
+    # arts = [Art.objects.get(pk=x['art']) for x in books.values('art') if x['art'] != None]
+    # if Art.objects.get(pk=books.values('art')['art']) != None:
+    #     arts = [Art.objects.get(pk=x['art']) for x in books.values('art')]
 
-	return render(request, 'about/book.html', {
-		'books': books,
-		# 'arts': arts
-	})
+    return render(request, 'about/book.html', {
+        'books': books,
+        # 'arts': arts
+    })
 
 
 def Index(request):
-	return render(request, 'index.html')
+    return render(request, 'index.html')
 
 
 def BookAdd(request):
-	if request.method == "POST":
-		form = AddBook(request.POST)
-		book = form.save(commit=False)
-		book.save()
-		return redirect('about-book', pk=book.slug)
-	else:
-		form = AddBook()
-		return render(request, 'about/add.html', {'form': form})
+    if request.method == "POST":
+        form = AddBook(request.POST)
+        book = form.save(commit=False)
+        book.save()
+        return redirect('about-book', pk=book.slug)
+    else:
+        form = AddBook()
+        return render(request, 'about/add.html', {'form': form})
 
 
 # def search(request):
@@ -44,21 +44,23 @@ def BookAdd(request):
 #         return render(request,"search/console.html",{})
 
 def Search(request):
-	return render_to_response('search.html')
+    return render_to_response('search.html')
 
 
 def SearchResult(request):
-	if 'q' in request.GET and request.GET['q']:
-		q = request.GET['q']
-		books = Book.objects.filter(name=q)
-		return render_to_response('search_results.html',
-								  {'books': books, 'query': q})
-	else:
-		return HttpResponse('Please submit a search term.')
+    if 'q' in request.GET and request.GET['q']:
+        q = request.GET['q'].capitalize()
+        books = Book.objects.filter(name__icontains=q)
+        half = Book.objects.filter(name__istartswith=q[:int(len(q)/2)])
+        books = books | half
+        return render_to_response('search_results.html',
+                                  {'books': books, 'query': q})
+    else:
+        return HttpResponse('Please submit a search term.')
 
 
 def bad_search(request):
-	# The following line will raise KeyError if 'q' hasn't
-	# been submitted!
-	message = 'You searched for: %r' % request.GET['q']
-	return HttpResponse(message)
+    # The following line will raise KeyError if 'q' hasn't
+    # been submitted!
+    message = 'You searched for: %r' % request.GET['q']
+    return HttpResponse(message)
