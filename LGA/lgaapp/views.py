5417915d40d0e40526bf9fae336340e279e4e2b7
Medezee <gorkabrusentsov@gmail.com>
2019-04-04 12:44:31 +0300

    Fixed search func
    Made search work now as planned.

--- LGA/lgaapp/views.py
+++ LGA/lgaapp/views.py
@@ -58,43 +58,82 @@ def SearchResult(request):
     if request.GET:
         q = dict(request.GET)
         q = dict(map(cap, list(zip(list(q.keys()), q.values()))))
+
         try:
-            q['ser'] = int(q['ser'])
+            if q['ser'] == '':
+                q['ser'] = -1
+            else:
+                q['ser'] = int(q['ser'])
         except:
             pass
 
         try:
-            q['invnum'] = int(q['invnum'])
+            if q['invnum'] == '':
+                q['invnum'] = -1
+            else:
+                q['invnum'] = int(q['invnum'])
         except:
             pass
 
         try:
-            q['ciph'] = int(q['ciph'])
+            if q['ciph'] == '':
+                q['ciph'] = -1
+            else:
+                q['ciph'] = int(q['ciph'])
         except:
             pass
 
-        books = Book.objects.filter(name__icontains=q['n'],
-                                    author__icontains=q['a']
-                                    # publication__icontains=,
-                                    # description__icontains=,
-                                    # series__icontains=,
-                                    # personality__icontains=,
-                                    # additional__icontains=,
-                                    # isbn__icontains=,
-                                    # inventory_number__icontains=,
-                                    # cipher__icontains=,
-                                    # year__icontains=,
-                                    # place__icontains=,
-                                    # language__icontains=,
-                                    # country__icontains=,
-                                    # subject__icontains=,
-                                    # art__icontains=,
-                                    # group__icontains=
-                                    )
+        years = []
+        try:
+            for i in range(int(q['yfr']), int(q['yto'])+1):
+                years.append(str(i))
+        except:
+            pass
 
-        half = Book.objects.filter(name__istartswith=q['n'][:int(len(q) / 2)],
-                                   author__istartswith=q['a'][:int(len(q) / 2)])
-        books = books | half
+        books = Book.objects.all()
+
+        books = books.filter(name__icontains=q['n'],
+                            author__icontains=q['a'],
+                            publication__icontains=q['publ'],
+                            description__icontains=q['desc'],
+                            personality__icontains=q['pers'],
+                            place__icontains=q['plc'],
+                            language__icontains=q['lang'],
+                            country__icontains=q['cntr']
+                            )
+
+        if q['n'] != '':
+            half = Book.objects.filter(name__istartswith=q['n'][:int(len(q) / 2)])
+            books = books | half
+
+        if q['a'] != '':
+            half = Book.objects.filter(author__istartswith=q['a'][:int(len(q) / 2)])
+            books = books | half
+
+        if q['ser'] != -1:
+            books = books.filter(series=q['ser'])
+
+        if q['invnum'] != -1:
+            books = books.filter(inventory_number=q['invnum'])
+
+        if q['ciph'] != -1 :
+            books = books.filter(cipher=q['ciph'])
+
+        if years:
+            books = books.filter(year__in=years)
+
+        if q['isbn'] != '':
+            books = books.filter(isbn__iexact=q['isbn'])
+
+        if q['subj'] != '':
+            books = books.filter(subject__name=q['subj'])
+
+        if q['art'] != '':
+            books = books.filter(art__name=q['art'])
+
+        if q['gr'] != '':
+            books = books.filter(group__name=q['gr'])
+
         return render_to_response('search/results.html',
                                   {'books': books, 'query': q})
     else:
