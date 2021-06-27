83b4e73989669268a1b7e2c7700efaaf3217081c
vladtsap <vldzpp@gmail.com>
2021-06-27 17:33:47 +0300

    updated URL paths

--- LGA/urls.py
+++ LGA/urls.py
@@ -15,9 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from django.conf.urls import url
 
 urlpatterns = [
 	path('admin/', admin.site.urls),
-	url(r'', include('lgaapp.urls')),
+	path('', include('lgaapp.urls')),
 ]
--- lgaapp/urls.py
+++ lgaapp/urls.py
@@ -1,12 +1,12 @@
-from django.conf.urls import url
+from django.urls import path
+
 from . import views
 
 urlpatterns = [
-	url(r'^$', views.index, name='index'),
-	url(r'^about/(?P<pk>(.*?)+)/$', views.book_view, name='about-book'),
-	url(r'^about/(?P<pk>(.*?)+)/edit', views.book_edit, name='edit-book'),
-	url(r'^about/(?P<pk>(.*?)+)/delete', views.book_delete, name='delete-book'),
-	url(r'^add/$', views.book_add, name='add-book'),
-	url(r'^search/$', views.search_box, name='search-book'),
-	url(r'^search/result/$', views.search_result, name='search-result'),
+	path('', views.index, name='index'),
+	path('about/<int:pk>/', views.book_view_delete, name='about-book'),
+	path('about/<int:pk>/edit/', views.book_edit, name='edit-book'),
+	path('add/', views.book_add, name='add-book'),
+	path('search/', views.search_box, name='search-book'),
+	path('search/result/', views.search_result, name='search-result'),
 ]
